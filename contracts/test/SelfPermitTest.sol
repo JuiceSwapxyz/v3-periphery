7fe8e3be36da6c268d9ff45fd1d2a8a3e6a09691
lapatric <42653152+lapatric@users.noreply.github.com>
2026-02-24 18:29:08 +0100

    Revert "Fix code style issues with Prettier"
    This reverts commit 18adc03d59d78acdffb337c1b658134e87b07f61.

--- contracts/test/SelfPermitTest.sol
+++ contracts/test/SelfPermitTest.sol
@@ -4,4 +4,6 @@ pragma solidity =0.7.6;
 import '../base/SelfPermit.sol';
 
 /// @dev Same as SelfPermit but not abstract
-contract SelfPermitTest is SelfPermit {}
+contract SelfPermitTest is SelfPermit {
+
+}
